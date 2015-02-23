a9796283f015b84e98adf746b86e7aade758283e
Mario Lameiras <mario.lameiras@gmail.com>
2015-02-23 10:13:47 +0000

    Changed url to https

--- js/app.js
+++ js/app.js
@@ -54,7 +54,7 @@
 	function drawBubbles(m) {
 
 		// generate data with calculated layout values
-		d3.xhr("http://nodeproxy.eu-gb.mybluemix.net/?url=https://api.bitreserve.org/v0/reserve/statistics","application/json", function(error, root) {
+		d3.xhr("https://nodeproxy.eu-gb.mybluemix.net/?url=https://api.bitreserve.org/v0/reserve/statistics","application/json", function(error, root) {
 
 			var nodes = bubble.nodes(processData(JSON.parse(root.response)))
 			.filter(function(d) {
