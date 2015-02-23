10554bcb7d4ca296849891c13758fe606e8595c5
Mario Lameiras <mario.lameiras@gmail.com>
2015-02-23 00:57:46 +0000

    Added online proxy

--- js/app.js
+++ js/app.js
@@ -61,7 +61,7 @@
 	function drawBubbles(m) {
 
 		// generate data with calculated layout values
-		d3.xhr("http://localhost:8080/api/bitreserve","application/json", function(error, root) {
+		d3.xhr("http://nodeproxy.eu-gb.mybluemix.net/?url=https://api.bitreserve.org/v0/reserve/statistics","application/json", function(error, root) {
 
 			var nodes = bubble.nodes(processData(JSON.parse(root.response)))
 			.filter(function(d) {
